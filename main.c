d8869b17eec911b2f54d624e1c3751bef2e7a54f
mateussiilva <mateuspython3010@gmail.com>
2023-06-08 18:10:35 -0300

    Começando o jogo

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main(void){
-  printf("Jogo de advinhação");
+int main(){
+  printf("---------------------------------\n");
+  printf(" BEM VINDO AO JOGO DA ADVINHAÇÃO\n");
+  printf("---------------------------------\n");
+  int chute, numeroSecreto = 10;
+  for(int i = 1; i <= 3; i++){
+    scanf("%d",chute);
+    int acertou = chute == numeroSecreto;
+    if (acertou){
+      printf("Parabens você acertou");
+      i = 3;
+    }else{
+      int maior = chute > numeroSecreto;
+      if(maior){
+        printf("O");
+      }
+    }
+  }
+
   return 0;
 }
 
